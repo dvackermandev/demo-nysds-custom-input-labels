30cc41405d2c0215cd06505f441d5d8ce852b5f7
David Ackerman <david.ackerman@its.ny.gov>
2026-03-06 10:13:30 -0500

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import './styles/global.css'
 import Message from './Message';
 
 const defaultErrors = {
-    emptyDocType: false,
-    emptyBeginDate: false,
-    futureBeginDate: false,
-    beginDateGreaterThanEndDate: false,
-    emptyEndDate: false,
-    futureEndDate: false,
-    endDateLessThanBeginDate: false,
-    emptyNotice: false,
-    noData: false,
-    errorResponse: false,
-    serviceUnavailable: false
-  }
+  emptyDocType: false,
+  emptyBeginDate: false,
+  futureBeginDate: false,
+  beginDateGreaterThanEndDate: false,
+  emptyEndDate: false,
+  futureEndDate: false,
+  endDateLessThanBeginDate: false,
+  emptyNotice: false,
+  noData: false,
+  errorResponse: false,
+  serviceUnavailable: false
+}
 function App() {
   const [isGenerating, setIsGenerating] = useState(false)
   const [errors, setErrors] = useState(defaultErrors)
@@ -35,7 +35,7 @@ function App() {
   useEffect(() => {
     if (selectTypeRef.current) {
       selectTypeRef.current.addEventListener('nys-change', (event: any) => {
-        setErrors(prev => ({...prev, emptyDocType: false}))
+        setErrors(prev => ({ ...prev, emptyDocType: false }))
         setDocType(event.detail.value)
       })
     }
@@ -54,11 +54,11 @@ function App() {
   useEffect(() => {
     if (beginDatepickerRef.current) {
       beginDatepickerRef.current.addEventListener('nys-blur', (event: any) => {
-        setErrors(prev => ({...prev, emptyBeginDate: false, futureBeginDate: false, beginDateGreaterThanEndDate: false}))
+        setErrors(prev => ({ ...prev, emptyBeginDate: false, futureBeginDate: false, beginDateGreaterThanEndDate: false }))
         setBegin(event.target.value)
       })
       beginDatepickerRef.current.addEventListener('nys-input', (event: any) => {
-        setErrors(prev => ({...prev, emptyBeginDate: false, futureBeginDate: false, beginDateGreaterThanEndDate: false}))
+        setErrors(prev => ({ ...prev, emptyBeginDate: false, futureBeginDate: false, beginDateGreaterThanEndDate: false }))
         setBegin(event.target.value)
       })
     }
@@ -68,11 +68,11 @@ function App() {
   useEffect(() => {
     if (endDatepickerRef.current) {
       endDatepickerRef.current.addEventListener('nys-blur', (event: any) => {
-        setErrors(prev => ({...prev, emptyEndDate: false, futureEndDate: false, endDateLessThanBeginDate: false}))
+        setErrors(prev => ({ ...prev, emptyEndDate: false, futureEndDate: false, endDateLessThanBeginDate: false }))
         setEnd(event.target.value)
       })
       endDatepickerRef.current.addEventListener('nys-input', (event: any) => {
-        setErrors(prev => ({...prev, emptyEndDate: false, futureEndDate: false, endDateLessThanBeginDate: false}))
+        setErrors(prev => ({ ...prev, emptyEndDate: false, futureEndDate: false, endDateLessThanBeginDate: false }))
         setEnd(event.target.value)
       })
     }
@@ -82,7 +82,7 @@ function App() {
   useEffect(() => {
     if (selectNoticeRef.current) {
       selectNoticeRef.current.addEventListener('nys-change', (event: any) => {
-        setErrors(prev => ({...prev, emptyNotice: false}))
+        setErrors(prev => ({ ...prev, emptyNotice: false }))
         setSelectedNotice(event.detail.value)
       })
     }
@@ -107,10 +107,10 @@ function App() {
     await delay(2000)
     setIsGenerating(false)
     const randomInt = Math.floor(Math.random() * (4 - 1 + 1) + 1)
-    if (randomInt === 1) {setSuccess(true)}
-    else if (randomInt === 2) {setErrors(prev => ({...prev, noData: true}))}
-    else if (randomInt === 3) {setErrors(prev => ({...prev, errorResponse: true}))}
-    else if (randomInt === 4) {setErrors(prev => ({...prev, serviceUnavailable: true}))}
+    if (randomInt === 1) { setSuccess(true) }
+    else if (randomInt === 2) { setErrors(prev => ({ ...prev, noData: true })) }
+    else if (randomInt === 3) { setErrors(prev => ({ ...prev, errorResponse: true })) }
+    else if (randomInt === 4) { setErrors(prev => ({ ...prev, serviceUnavailable: true })) }
 
   }
   const checkIfInvalid = () => {
@@ -118,129 +118,131 @@ function App() {
     const emptyNotice = isDocument && !selectedNotice
     const emptyBeginDate = isReport && !begin
     const emptyEndDate = isReport && !end
-    const futureBeginDate = isReport  && !!begin && new Date(begin) > new Date()
-    const futureEndDate = isReport  && !!end && new Date(end) > new Date()
+    const futureBeginDate = isReport && !!begin && new Date(begin) > new Date()
+    const futureEndDate = isReport && !!end && new Date(end) > new Date()
     const beginDateGreaterThanEndDate = isReport && !!begin && !!end && new Date(begin) > new Date(end)
     const endDateLessThanBeginDate = isReport && !!begin && !!end && new Date(end) < new Date(begin)
-    setErrors(prev => ({...prev, emptyDocType, emptyNotice, emptyBeginDate, emptyEndDate, futureBeginDate, futureEndDate, beginDateGreaterThanEndDate ,endDateLessThanBeginDate}))
+    setErrors(prev => ({ ...prev, emptyDocType, emptyNotice, emptyBeginDate, emptyEndDate, futureBeginDate, futureEndDate, beginDateGreaterThanEndDate, endDateLessThanBeginDate }))
     return (emptyDocType || emptyNotice || emptyBeginDate || emptyEndDate || futureBeginDate || futureEndDate || beginDateGreaterThanEndDate || endDateLessThanBeginDate)
   }
 
   const invalidDateError = 'Please enter a valid value. The field is incomplete or has an invalid date.'
   return (
-    <div className="outer-wrap">
-      <div className="nys-grid-row nys-margin-b-250 sm:mb-4">
-        <div className="nys-tablet:nys-grid-col-6">
-          <div className="input-label-wrap nys-tablet:nys-flex-justify-end">
-            <label htmlFor="select-type" className={`input-label ${errors.emptyDocType && 'error'} ${isGenerating && 'disabled'}`}>Type*</label>
-          </div>
-        </div>
-        <div className="nys-tablet:nys-grid-col-6">
-          <nys-select
-            ref={selectTypeRef}
-            value={docType}
-            id="select-type"
-            disabled={isGenerating}
-            showError={errors.emptyDocType}
-            errorMessage={errors.emptyDocType ? "Document type is required." : ""}
-          >
-            {demoData.reportTypes.map((item, i) => {
-              return <optgroup key={i} label={item.label}>{item.items.map((item, i) => { return <option key={i} value={item.value}>{item.label}</option> })}</optgroup>
-            })}
-          </nys-select>
-        </div>
-      </div>
-      {(isReport || isSos) && <>
-        <div className="nys-grid-row nys-margin-b-250">
-          <div className="nys-tablet:nys-grid-col-6">
-            <div className="input-label-wrap toggle nys-tablet:nys-flex-justify-end">
-              <label htmlFor='docgen-toggle' className={`input-label ${isGenerating && 'disabled'}`}>All Dates</label>
-            </div>
-          </div>
-          <div className="nys-tablet:nys-grid-col-6">
-            <nys-toggle ref={toggleRef} id="docgen-toggle" checked={allDates || isSos ? true : undefined} disabled={isGenerating || isSos ? true : undefined} size="md" icon={false} noIcon></nys-toggle>
-          </div>
-        </div>
-        <div className="nys-grid-row nys-margin-b-250">
+    <form>
+      <div className="outer-wrap">
+        <div className="nys-grid-row nys-margin-b-250 sm:mb-4">
           <div className="nys-tablet:nys-grid-col-6">
             <div className="input-label-wrap nys-tablet:nys-flex-justify-end">
-              <label htmlFor='begin-datepicker' className={`input-label ${(errors.emptyBeginDate || errors.futureBeginDate || errors.beginDateGreaterThanEndDate) && 'error'} ${(allDates || isGenerating || isSos) && 'disabled'}`}>Begin*</label>
+              <label htmlFor="select-type" className={`input-label ${errors.emptyDocType && 'error'} ${isGenerating && 'disabled'}`}>Type*</label>
             </div>
           </div>
           <div className="nys-tablet:nys-grid-col-6">
-            <nys-datepicker
-              showError={errors.emptyBeginDate || errors.futureBeginDate || errors.beginDateGreaterThanEndDate}
-              errorMessage={
-                (errors.emptyBeginDate && "Begin date is required" ||
-                  errors.futureBeginDate && "Future begin date is not allowed." ||
-                  errors.beginDateGreaterThanEndDate && "Begin date must be before end date.") ||
-                invalidDateError
-              }
-              ref={beginDatepickerRef}
-              value={begin ? begin : undefined}
-              disabled={allDates || isGenerating || isSos}
-              width="full"
-              id="begin-datepicker"
-              name="begin-datepicker"
-            ></nys-datepicker>
+            <nys-select
+              ref={selectTypeRef}
+              value={docType}
+              id="select-type"
+              disabled={isGenerating}
+              showError={errors.emptyDocType}
+              errorMessage={errors.emptyDocType ? "Document type is required." : ""}
+            >
+              {demoData.reportTypes.map((item, i) => {
+                return <optgroup key={i} label={item.label}>{item.items.map((item, i) => { return <option key={i} value={item.value}>{item.label}</option> })}</optgroup>
+              })}
+            </nys-select>
           </div>
         </div>
-        <div className="nys-grid-row nys-margin-b-250 ">
+        {(isReport || isSos) && <>
+          <div className="nys-grid-row nys-margin-b-250">
+            <div className="nys-tablet:nys-grid-col-6">
+              <div className="input-label-wrap toggle nys-tablet:nys-flex-justify-end">
+                <label htmlFor='docgen-toggle' className={`input-label ${isGenerating && 'disabled'}`}>All Dates</label>
+              </div>
+            </div>
+            <div className="nys-tablet:nys-grid-col-6">
+              <nys-toggle ref={toggleRef} id="docgen-toggle" checked={allDates || isSos ? true : undefined} disabled={isGenerating || isSos ? true : undefined} size="md" icon={false} noIcon></nys-toggle>
+            </div>
+          </div>
+          <div className="nys-grid-row nys-margin-b-250">
+            <div className="nys-tablet:nys-grid-col-6">
+              <div className="input-label-wrap nys-tablet:nys-flex-justify-end">
+                <label htmlFor='begin-datepicker' className={`input-label ${(errors.emptyBeginDate || errors.futureBeginDate || errors.beginDateGreaterThanEndDate) && 'error'} ${(allDates || isGenerating || isSos) && 'disabled'}`}>Begin*</label>
+              </div>
+            </div>
+            <div className="nys-tablet:nys-grid-col-6">
+              <nys-datepicker
+                showError={errors.emptyBeginDate || errors.futureBeginDate || errors.beginDateGreaterThanEndDate}
+                errorMessage={
+                  (errors.emptyBeginDate && "Begin date is required" ||
+                    errors.futureBeginDate && "Future begin date is not allowed." ||
+                    errors.beginDateGreaterThanEndDate && "Begin date must be before end date.") ||
+                  invalidDateError
+                }
+                ref={beginDatepickerRef}
+                value={begin ? begin : undefined}
+                disabled={allDates || isGenerating || isSos}
+                width="full"
+                id="begin-datepicker"
+                name="begin-datepicker"
+              ></nys-datepicker>
+            </div>
+          </div>
+          <div className="nys-grid-row nys-margin-b-250 ">
+            <div className="nys-tablet:nys-grid-col-6">
+              <div className="input-label-wrap nys-tablet:nys-flex-justify-end">
+                <label htmlFor='end-datepicker' className={`input-label ${(errors.emptyEndDate || errors.futureEndDate || errors.endDateLessThanBeginDate) && 'error'} ${(allDates || isGenerating || isSos) && 'disabled'}`}>End*</label>
+              </div>
+            </div>
+            <div className="nys-tablet:nys-grid-col-6">
+              <nys-datepicker
+                showError={errors.emptyEndDate || errors.futureEndDate || errors.endDateLessThanBeginDate}
+                errorMessage={
+                  (errors.emptyEndDate && "End date is required" ||
+                    errors.futureEndDate && "Future end date is not allowed." ||
+                    errors.endDateLessThanBeginDate && "End date must be after begin date.") ||
+                  invalidDateError
+                }
+                ref={endDatepickerRef}
+                value={end ? end : undefined}
+                disabled={allDates || isGenerating || isSos}
+                width="full"
+                id="end-datepicker"
+                name="end-datepicker"
+                className={`${errors.emptyEndDate && 'p-invalid'}`}
+              ></nys-datepicker>
+            </div>
+          </div>
+        </>}
+        {isDocument && <div className="nys-grid-row nys-margin-b-250 sm:mb-4">
           <div className="nys-tablet:nys-grid-col-6">
             <div className="input-label-wrap nys-tablet:nys-flex-justify-end">
-              <label htmlFor='end-datepicker' className={`input-label ${(errors.emptyEndDate || errors.futureEndDate || errors.endDateLessThanBeginDate) && 'error'} ${(allDates || isGenerating || isSos) && 'disabled'}`}>End*</label>
+              <label htmlFor='select-notice' className={`input-label ${errors.emptyNotice && 'error'} ${isGenerating && 'disabled'}`}>Month &amp; Year*</label>
             </div>
           </div>
           <div className="nys-tablet:nys-grid-col-6">
-            <nys-datepicker
-              showError={errors.emptyEndDate || errors.futureEndDate || errors.endDateLessThanBeginDate}
-              errorMessage={
-                (errors.emptyEndDate && "End date is required" ||
-                  errors.futureEndDate && "Future end date is not allowed." ||
-                  errors.endDateLessThanBeginDate && "End date must be after begin date.") ||
-                invalidDateError
-              }
-              ref={endDatepickerRef}
-              value={end ? end : undefined}
-              disabled={allDates || isGenerating || isSos}
-              width="full"
-              id="end-datepicker"
-              name="end-datepicker"
-              className={`${errors.emptyEndDate && 'p-invalid'}`}
-            ></nys-datepicker>
+            <nys-select
+              showError={errors.emptyNotice}
+              errorMessage={errors.emptyNotice ? "Month & Year is required." : ""}
+              ref={selectNoticeRef}
+              id="select-notice"
+              value={selectedNotice}
+              disabled={isGenerating}>
+              {demoData.monthlyNotices.map((item, i) => {
+                return <option key={i} value={item.value}>{item.label}</option>
+              })}
+            </nys-select>
           </div>
-        </div>
-      </>}
-      {isDocument && <div className="nys-grid-row nys-margin-b-250 sm:mb-4">
-        <div className="nys-tablet:nys-grid-col-6">
-          <div className="input-label-wrap nys-tablet:nys-flex-justify-end">
-            <label htmlFor='select-notice' className={`input-label ${errors.emptyNotice && 'error'} ${isGenerating && 'disabled'}`}>Month &amp; Year*</label>
-          </div>
-        </div>
-        <div className="nys-tablet:nys-grid-col-6">
-          <nys-select
-            showError={errors.emptyNotice}
-            errorMessage={errors.emptyNotice ? "Month & Year is required." : ""}
-            ref={selectNoticeRef}
-            id="select-notice"
-            value={selectedNotice}
-            disabled={isGenerating}>
-            {demoData.monthlyNotices.map((item, i) => {
-              return <option key={i} value={item.value}>{item.label}</option>
-            })}
-          </nys-select>
-        </div>
-      </div>}
-      {success && <Message nysType='success' className="response-message">Success!</Message>}
-      {errors.noData && <Message nysType='danger' className="response-message">No data found.</Message>}
-      {errors.errorResponse && <Message nysType='danger' className="response-message">An error occurred while processing the request.</Message>}
-      {errors.serviceUnavailable && <Message nysType='danger' className="response-message">Service is not available.</Message>}
+        </div>}
+        {success && <Message nysType='success' className="response-message">Success!</Message>}
+        {errors.noData && <Message nysType='danger' className="response-message">No data found.</Message>}
+        {errors.errorResponse && <Message nysType='danger' className="response-message">An error occurred while processing the request.</Message>}
+        {errors.serviceUnavailable && <Message nysType='danger' className="response-message">Service is not available.</Message>}
 
-      <div className='nys-display-flex'>
-        <nys-button className="nys-margin-r-200" onClick={clearForm} label="Clear" variant="outline" disabled={isGenerating} fullWidth></nys-button>
-        <nys-button onClick={handleGenerate} label="Generate" disabled={isGenerating} fullWidth></nys-button>
+        <div className='nys-display-flex'>
+          <nys-button className="nys-margin-r-200" onClick={clearForm} label="Clear" variant="outline" disabled={isGenerating} fullWidth></nys-button>
+          <nys-button onClick={handleGenerate} label="Generate" disabled={isGenerating} fullWidth></nys-button>
+        </div>
       </div>
-    </div>)
+    </form>)
 }
 
 export default App
