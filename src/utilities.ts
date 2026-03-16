f6eba022a66ba45e6158fa4fa206320e3095ee33
dvackermandev <david.ackerman@its.ny.gov>
2026-03-15 20:19:18 -0400

    Add types for useEventListener args

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-export function useEventListener(targetElement: any, eventName: any, eventHandler: any) {
+export function useEventListener(targetElement: React.RefObject<any>, eventName: string, eventHandler: (event: any) => void) {
   useEffect(() => {
     const element = targetElement.current
     if(!element) return
